7fbac3acf40e3f87dc75f4f0f3438813370ef6f4
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2023-04-02 02:46:32 +0100

    0.0.2

--- test/server.js
+++ test/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const ejsRenderer = require('../index');
+// const ejsRenderer = require('../index');
+const ejsRenderer = require('ejs-with-layouts');
 const path = require('path');
 
 const app = express();
@@ -8,6 +9,7 @@ const port = 4000;
 app.engine('ejs', ejsRenderer);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('*', (req, res) => {
 	res.render('home', {
